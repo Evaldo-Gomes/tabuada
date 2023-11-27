958be7fcfd8e63d6c9fa86285bc900db75f5533d
Evaldo-Gomes <evaldo_gomes@hotmail.com>
2023-11-27 15:54:40 -0300

    - Commit de teste

---
+++ tabuada.py
@@ -0,0 +1,34 @@
+#Cálculo da tabuada
+
+#Apresentação
+print('\n\t\t\t  --  Cálculo da Tabuada   --')
+
+# entradas
+n = int(input('Informe o número: '))
+
+# processamento
+n0 = 0
+n1 = 1*n
+n2 = 2*n
+n3 = 3*n
+n4 = 4*n
+n5 = 5*n
+n6 = 6*n
+n7 = 7*n
+n8 = 8*n
+n9 = 9*n
+n10 = 10*n
+
+#saída
+print(f'\t 0 x {n} = {n0}')
+print(f'\t 1 x {n} = {n1}')
+print(f'\t 2 x {n} = {n2}')
+print(f'\t 3 x {n} = {n3}')
+print(f'\t 4 x {n} = {n4}')
+print(f'\t 5 x {n} = {n5}')
+print(f'\t 6 x {n} = {n6}')
+print(f'\t 7 x {n} = {n7}')
+print(f'\t 8 x {n} = {n8}')
+print(f'\t 9 x {n} = {n9}')
+print(f'\t10 x {n} = {n10}')
+# print(f'\t11 x {n} = {n11}')
